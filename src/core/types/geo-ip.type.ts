b5f3b17ab3e195058252a95e0d61505aa3cd697c
GuilhermeSantos001 <luizgp120@hotmail.com>
2022-07-13 00:09:53 -0300

    feat(core-types): added new types for updates

---
+++ src/core/types/geo-ip.type.ts
@@ -0,0 +1,7 @@
+export type GeoIP = {
+  timezone: string;
+  country: string;
+  city: string;
+  region: string;
+  ipAddress: string;
+};
---
+++ src/core/types/niro-config.type.ts
@@ -0,0 +1,7 @@
+export type NiroConfig = {
+  locale: {
+    main: string;
+    languages: string[];
+    path: string;
+  };
+};
