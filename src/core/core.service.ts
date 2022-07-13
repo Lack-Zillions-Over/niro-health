53af328a1e581a1d1efb2320af5c1551bef4e82e
GuilhermeSantos001 <luizgp120@hotmail.com>
2022-07-12 23:52:33 -0300

    refactor(core): it is now possible to get Geo-location of users
    It is now possible to get Geo-location of users. Security change in private key methods

--- src/core/core.service.ts
+++ src/core/core.service.ts
@@ -1,38 +1,68 @@
 import { Injectable } from '@nestjs/common';
 
-import { CreatePrivateKeyDto } from './dto/create-private-keys.dto';
-import { UpdatePrivateKeyDto } from './dto/update-private-keys.dto';
+import { Request } from 'express';
+import { getClientIp } from 'request-ip';
+import { lookup } from 'geoip-lite';
 
-import { CreatePrivateKeyFactory } from './factories/create-private-keys.factory';
-import { ValidatePrivateKeyFactory } from './factories/validate-private-keys.factory';
-import { FindAllPrivateKeysFactory } from './factories/find-all-private-keys.factory';
-import { FindByTagPrivateKeyFactory } from './factories/find-by-tag-private-keys.factory';
-import { UpdatePrivateKeyFactory } from './factories/update-private-keys.factory';
-import { RemovePrivateKeyFactory } from './factories/remove-private-keys.factory';
+import { CreatePrivateKeyDto } from '@/core/dto/create-private-keys.dto';
+import { UpdatePrivateKeyDto } from '@/core/dto/update-private-keys.dto';
+
+import { CreatePrivateKeyFactory } from '@/core/factories/create-private-keys.factory';
+import { ValidatePrivateKeyFactory } from '@/core/factories/validate-private-keys.factory';
+import { FindAllPrivateKeysFactory } from '@/core/factories/find-all-private-keys.factory';
+import { FindByTagPrivateKeyFactory } from '@/core/factories/find-by-tag-private-keys.factory';
+import { UpdatePrivateKeyFactory } from '@/core/factories/update-private-keys.factory';
+import { RemovePrivateKeyFactory } from '@/core/factories/remove-private-keys.factory';
+
+import { LocaleService } from '@/core/i18n/i18n.service';
 
 @Injectable()
 export class CoreService {
+  constructor(private readonly localeService: LocaleService) {}
+  async getClientGeoIP(req: Request) {
+    const ipAddress = getClientIp(req);
+
+    return {
+      ...lookup(ipAddress),
+      device_name: req.headers['user-agent'],
+      ipAddress,
+    };
+  }
+
+  async getCookie(req: Request, name: string) {
+    return req.cookies[name] as string;
+  }
+
   async createPrivateKey(key: CreatePrivateKeyDto) {
-    return await CreatePrivateKeyFactory.run(key);
+    return await CreatePrivateKeyFactory.run(key, this.localeService.locale);
   }
 
   async validatePrivateKey(tag: string, secret: string, value: string) {
-    return await ValidatePrivateKeyFactory.run(tag, secret, value);
+    return await ValidatePrivateKeyFactory.run(
+      tag,
+      secret,
+      value,
+      this.localeService.locale,
+    );
   }
 
   async findAllPrivateKeys() {
-    return await FindAllPrivateKeysFactory.run();
+    return await FindAllPrivateKeysFactory.run(this.localeService.locale);
   }
 
   async findByTagPrivateKey(tag: string) {
-    return await FindByTagPrivateKeyFactory.run(tag);
+    return await FindByTagPrivateKeyFactory.run(tag, this.localeService.locale);
   }
 
   async updatePrivateKey(id: string, newData: UpdatePrivateKeyDto) {
-    return await UpdatePrivateKeyFactory.run(id, newData);
+    return await UpdatePrivateKeyFactory.run(
+      id,
+      newData,
+      this.localeService.locale,
+    );
   }
 
   async removePrivateKey(id: string) {
-    return await RemovePrivateKeyFactory.run(id);
+    return await RemovePrivateKeyFactory.run(id, this.localeService.locale);
   }
 }
