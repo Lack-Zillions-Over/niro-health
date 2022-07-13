adda0d015980b6a41c7099c2b9a6c680ad2459ac
GuilhermeSantos001 <luizgp120@hotmail.com>
2022-07-13 00:06:07 -0300

    refactor(middlewares): improving the security of application routes
    You can now use a master key or private key to access the application's routes.
    
    BREAKING CHANGE: Previous endpoints may not work due to the security-critical change in the
    application's routes.

--- src/core/middlewares/authorization.middleware.ts
+++ src/core/middlewares/authorization.middleware.ts
@@ -4,20 +4,55 @@ import {
   Injectable,
   NestMiddleware,
 } from '@nestjs/common';
+
 import { Request, Response, NextFunction } from 'express';
-import { StringEx } from '../utils/string-ex.util';
+
+import { ValidatePrivateKeyFactory } from '@/core/factories/validate-private-keys.factory';
+
+import { LocaleService } from '@/core/i18n/i18n.service';
+import { StringEx } from '@/core/utils/string-ex.util';
 
 @Injectable()
 export class AuthorizationMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    const headers = req.headers;
+  constructor(private readonly localeService: LocaleService) {}
+
+  private async _invalidMasterKey(authorization: string) {
+    if (StringEx.Hash(process.env.MASTER_KEY, 'sha1', 'hex') !== authorization)
+      return true;
+
+    return false;
+  }
+
+  private async _invalidPrivateKey(tag: string, secret: string, value: string) {
+    const key = await ValidatePrivateKeyFactory.run(
+      tag as string,
+      secret as string,
+      value as string,
+      this.localeService.locale,
+    );
+
+    if (!key || key instanceof Error) return true;
+
+    return false;
+  }
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const headers = req.headers,
+      authorization = headers.authorization,
+      tag = headers.key_tag as string,
+      secret = headers.key_secret as string,
+      value = headers.key_value as string;
 
     if (
-      StringEx.Hash(process.env.MASTER_KEY, 'sha1', 'hex') !==
-      headers['authorization']
+      ((await this._invalidMasterKey(authorization)) &&
+        (!tag || !secret || !value)) ||
+      ((await this._invalidMasterKey(authorization)) &&
+        (await this._invalidPrivateKey(tag, secret, value)))
     )
       throw new HttpException(
-        'API Key is invalid. Try again, later!',
+        this.localeService.translate(
+          'middlewares.authorization.exception',
+        ) as string,
         HttpStatus.UNAUTHORIZED,
       );
 
