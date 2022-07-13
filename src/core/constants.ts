f5f7329e7c2b295e520d59f81e23dfd74e0a24a7
GuilhermeSantos001 <luizgp120@hotmail.com>
2022-07-12 23:44:24 -0300

    feat(redis): added configuration for redis

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,3 +1,7 @@
-export const redisOptions = {
+import { RedisClientOptions } from 'redis';
+
+export const redisOptions: RedisClientOptions = {
   database: 1,
+  url: process.env.REDIS_HOST,
+  password: process.env.REDIS_PASSWORD,
 };
