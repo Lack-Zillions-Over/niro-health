87ee1c99ba73e58698397b89d248f73fb9bd1854
GuilhermeSantos001 <luizgp120@hotmail.com>
2022-07-13 00:16:01 -0300

    feat(users-controller): added auth methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,22 +5,33 @@ import {
   Get,
   Post,
   Body,
+  Req,
   Patch,
   Param,
   Delete,
 } from '@nestjs/common';
 
-import { UsersService } from './users.service';
-import { UsersParser } from './users.parser';
+import { Request } from 'express';
 
-import { CreateUserDto } from './dto/create-users.dto';
-import { UpdateUserDto } from './dto/update-users.dto';
+import { UsersService } from '@/users/users.service';
+import { CoreService } from '@/core/core.service';
+
+import { CreateUserDto } from '@/users/dto/create-users.dto';
+import { LoginUserDto } from '@/users/dto/login-users.dto';
+import { SessionValidateUserDto } from '@/users/dto/session-validate-users.dto';
+import { LogoutUserDto } from '@/users/dto/logout-users.dto';
+import { UpdateUserDto } from '@/users/dto/update-users.dto';
+
+import { UsersParser } from '@/users/parsers/users.parser';
+import { SessionParser } from '@/users/parsers/session.parser';
 
 @Controller('users')
 export class UsersController {
   constructor(
+    private readonly coreService: CoreService,
     private readonly usersService: UsersService,
     private readonly usersParser: UsersParser,
+    private readonly sessionParser: SessionParser,
   ) {}
 
   @Post()
@@ -31,6 +42,62 @@ export class UsersController {
       throw new HttpException(user.message, HttpStatus.FORBIDDEN);
 
     return this.usersParser.toJSON(user);
+  }
+
+  @Post('auth/login')
+  async login(@Body() loginUserDto: LoginUserDto, @Req() request: Request) {
+    const clientGeoIP = await this.coreService.getClientGeoIP(request);
+
+    const session = await this.usersService.login(
+      loginUserDto.username,
+      loginUserDto.password,
+      clientGeoIP.device_name,
+      clientGeoIP,
+    );
+
+    if (session instanceof Error)
+      throw new HttpException(session.message, HttpStatus.FORBIDDEN);
+
+    return this.sessionParser.toJSON(session);
+  }
+
+  @Post('auth/validate')
+  async sessionValidate(
+    @Body() sessionValidateUserDto: SessionValidateUserDto,
+    @Req() request: Request,
+  ) {
+    const clientGeoIP = await this.coreService.getClientGeoIP(request);
+
+    const session = await this.usersService.sessionValidate(
+      sessionValidateUserDto.username,
+      await this.coreService.getCookie(request, 'token_value'),
+      await this.coreService.getCookie(request, 'token_signature'),
+      await this.coreService.getCookie(request, 'token_revalidate_value'),
+      await this.coreService.getCookie(request, 'token_revalidate_signature'),
+      clientGeoIP.device_name,
+      clientGeoIP,
+    );
+
+    if (session instanceof Error)
+      throw new HttpException(session.message, HttpStatus.FORBIDDEN);
+
+    return true;
+  }
+
+  @Post('auth/logout')
+  async logout(@Body() logoutUserDto: LogoutUserDto, @Req() request: Request) {
+    const clientGeoIP = await this.coreService.getClientGeoIP(request);
+
+    const session = await this.usersService.logout(
+      logoutUserDto.username,
+      await this.coreService.getCookie(request, 'token_value'),
+      clientGeoIP.device_name,
+    );
+
+    if (session instanceof Error)
+      throw new HttpException(session.message, HttpStatus.FORBIDDEN);
+
+    return this.sessionParser.toJSON(session);
   }
 
   @Get()
