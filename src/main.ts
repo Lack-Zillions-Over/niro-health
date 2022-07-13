201a48033276030fc62153c775451c08cbccf0e4
GuilhermeSantos001 <luizgp120@hotmail.com>
2022-07-12 23:48:12 -0300

    feat(app): added support for cookies

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
+
+import fastifyCookie from '@fastify/cookie';
+
+import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  await app.register(fastifyCookie, {
+    secret: process.env.COOKIE_SECRET,
+  });
+
   await app.listen(4000, '0.0.0.0');
 }
 bootstrap();
