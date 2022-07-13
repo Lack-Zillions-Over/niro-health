45a72656eca7739bc47293448046abd67e43d80f
GuilhermeSantos001 <luizgp120@hotmail.com>
2022-07-13 00:17:09 -0300

    refactor(users-module): added support for locales and Change security methods

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,16 +1,16 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { UsersParser } from './users.parser';
-import { AuthorizationMiddleware } from '../core/middlewares/authorization.middleware';
+import { Module } from '@nestjs/common';
+
+import { UsersService } from '@/users/users.service';
+import { UsersController } from '@/users/users.controller';
+import { UsersParser } from '@/users/parsers/users.parser';
+import { SessionParser } from '@/users/parsers/session.parser';
+import { LocaleModule } from '@/core/i18n/i18n.module';
+import { CoreModule } from '@/core/core.module';
 
 @Module({
+  imports: [LocaleModule, CoreModule],
   controllers: [UsersController],
-  providers: [UsersService, UsersParser],
+  providers: [UsersService, UsersParser, SessionParser],
   exports: [UsersService],
 })
-export class UsersModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthorizationMiddleware).forRoutes('users');
-  }
-}
+export class UsersModule {}
