f3a43803fe1acdc1a9dc374c40ce0b1b7425cd6d
GuilhermeSantos001 <luizgp120@hotmail.com>
2022-11-05 16:56:22 -0300

    fix: Fixed import essentials modules

--- src/core/core.service.ts
+++ src/core/core.service.ts
@@ -4,8 +4,16 @@ import { Request } from 'express';
 import { getClientIp } from 'request-ip';
 import { lookup } from 'geoip-lite';
 
+import { LibsService } from '@/core/libs/libs.service';
+import { UtilsService } from '@/core/utils/utils.service';
+
 @Injectable()
 export class CoreService {
+  constructor(
+    public readonly libsService: LibsService,
+    public readonly utilsService: UtilsService,
+  ) {}
+
   async getClientGeoIP(req: Request) {
     const ipAddress = getClientIp(req);
 
