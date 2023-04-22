8ec419b1eccb94d18f3e2a1543dabcd9f178b034
GuilhermeSantos001 <luizgp120@hotmail.com>
2023-04-22 13:10:04 -0300

    fix(app-host): change property app to static

--- libs/app-host/src/app-host.service.ts
+++ libs/app-host/src/app-host.service.ts
@@ -1,26 +1,53 @@
 import { INestApplication, Injectable } from '@nestjs/common';
 
+/**
+ * @description The module that provides the access to the NestJS application instance.
+ */
 @Injectable()
 export class AppHostService {
-  app: INestApplication;
+  /**
+   * @description The NestJS application instance.
+   */
+  static app: INestApplication;
 
+  /**
+   * @description Set the NestJS application instance.
+   * @param app The NestJS application instance.
+   * @returns void
+   */
   setApp(app: INestApplication) {
-    this.app = app;
+    AppHostService.app = app;
   }
 
+  /**
+   * @description Returns the underlying native HTTP server.
+   * @returns {*}
+   */
   getHttpServer() {
-    return this.app.getHttpServer();
+    return AppHostService.app.getHttpServer();
   }
 
+  /**
+   * @description Returns the underlying HTTP adapter.
+   * @returns {HttpServer}
+   */
   getHttpAdapter() {
-    return this.app.getHttpAdapter();
+    return AppHostService.app.getHttpAdapter();
   }
 
+  /**
+   * @description Returns array of the microservices connected to the NestApplication.
+   * @returns {INestMicroservice[]}
+   */
   getMicroservices() {
-    return this.app.getMicroservices();
+    return AppHostService.app.getMicroservices();
   }
 
+  /**
+   * @description Returns the url the application is listening at, based on OS and IP version. Returns as an IP value either in IPv6 or IPv4
+   * @returns {Promise<string>} The IP where the server is listening
+   */
   getUrl() {
-    return this.app.getUrl();
+    return AppHostService.app.getUrl();
   }
 }
