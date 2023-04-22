de7d6c5effdd3a56e66c842bcf72f196371546dd
GuilhermeSantos001 <luizgp120@hotmail.com>
2023-04-22 07:16:05 -0300

    refactor(sqlite): this module has been moved to the core

--- libs/sqlite/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './sqlite.module';
-export * from './sqlite.service';
--- libs/sqlite/src/sqlite.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Database } from 'sqlite3';
-
-export declare namespace Sqlite {
-  export interface Class {
-    db: Database;
-  }
-}
