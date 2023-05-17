9a182b9d07109f45f70090b062e85f4a963d081b
GuilhermeSantos001 <luizgp120@hotmail.com>
2023-05-16 23:39:12 -0300

    docs(debug): writed Documentation of module

--- libs/debug/src/debug.service.ts
+++ libs/debug/src/debug.service.ts
@@ -1,24 +1,42 @@
 import { Injectable, Logger } from '@nestjs/common';
 import type { IDebugService } from '@app/debug';
 
+/**
+ * @description The module provides a wrapper for the NestJS Logger.
+ */
 @Injectable()
 export class DebugService implements IDebugService {
+  /**
+   * @description The method logs a message with the level `log`.
+   */
   public log(msg: string, ...optionalParams: [...any, string?]): void {
     Logger.log(msg, ...optionalParams);
   }
 
+  /**
+   * @description The method logs a message with the level `warn`.
+   */
   public warn(msg: string, ...optionalParams: [...any, string?]): void {
     Logger.warn(msg, ...optionalParams);
   }
 
+  /**
+   * @description The method logs a message with the level `error`.
+   */
   public error(msg: string, ...optionalParams: [...any, string?]): void {
     Logger.error(msg, ...optionalParams);
   }
 
+  /**
+   * @description The method logs a message with the level `debug`.
+   */
   public debug(msg: string, ...optionalParams: [...any, string?]): void {
     Logger.debug(msg, ...optionalParams);
   }
 
+  /**
+   * @description The method logs a message with the level `verbose`.
+   */
   public verbose(msg: string, ...optionalParams: [...any, string?]): void {
     Logger.verbose(msg, ...optionalParams);
   }
--- libs/debug/src/index.ts
+++ libs/debug/src/index.ts
@@ -1,3 +1,3 @@
+export * from './debug.interface';
 export * from './debug.module';
 export * from './debug.service';
-export * from './debug.interface';
