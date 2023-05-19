170b0346fcadd2a188fb41d206d1d45be21e0b27
GuilhermeSantos001 <luizgp120@hotmail.com>
2023-05-19 09:24:20 -0300

    docs(core): write documentation of module

--- libs/core/src/core.service.ts
+++ libs/core/src/core.service.ts
@@ -4,19 +4,31 @@ import { Request } from 'express';
 import { getClientIp } from 'request-ip';
 import { lookup } from 'geoip-lite';
 
+/**
+ * @description The module that provides methods for get client GeoIP and cookie.
+ */
 @Injectable()
 export class CoreService {
-  async getClientGeoIP(req: Request) {
-    const ipAddress = getClientIp(req);
+  /**
+   * @description Get client GeoIP.
+   * @param req Request object.
+   */
+  public async getClientGeoIP<T, P = Request | T>(req: P) {
+    const ipAddress = getClientIp(req as Request);
 
     return {
       ...lookup(ipAddress),
-      device_name: req.headers['user-agent'],
+      device_name: (req as Request).headers['user-agent'],
       ipAddress,
     };
   }
 
-  async getCookie(req: Request, name: string) {
-    return req.cookies[name] as string;
+  /**
+   * @description Get cookie by name.
+   * @param req Request object.
+   * @param name Cookie name.
+   */
+  public async getCookie<T, P = Request | T>(req: P, name: string) {
+    return (req as Request).cookies[name] as string;
   }
 }
